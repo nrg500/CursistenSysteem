9523ba618c7ebacb1aabde16b8f95d16124c7165
Berwout de Vries Robles <BerwoutV@Infosupport.com>
2017-11-10 14:50:39 +0100

    initial commit

---
+++ backend/src/main/java/nl/berwout/api/controllers/RegisterController.java
@@ -0,0 +1,12 @@
+package nl.berwout.api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+    public void postNewUser(String username, String password, String email){
+
+    }
+}
---
+++ backend/src/main/java/nl/berwout/api/models/NewUser.java
@@ -0,0 +1,7 @@
+package nl.berwout.api.models;
+
+public class NewUser {
+    private String username;
+    private String password;
+    private String email;
+}
