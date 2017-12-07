89e81043b783327f3ce47cd186f273930efbc916
Berwout de Vries Robles <BerwoutV@Infosupport.com>
2017-12-07 09:37:50 +0100

    working first version with h2db

--- backend/src/main/java/nl/berwout/api/models/NewUser.java
+++
@@ -1,7 +0,0 @@
-package nl.berwout.api.models;
-
-public class NewUser {
-    private String username;
-    private String password;
-    private String email;
-}
--- frontend/e2e/app.e2e-spec.ts
+++ frontend/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('website App', () => {
+describe('frontend App', () => {
   let page: AppPage;
 
   beforeEach(() => {
